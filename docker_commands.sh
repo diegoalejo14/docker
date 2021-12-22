12871b5818479bfafa3b9befb35572e701dcecc1
Alejandro Olivera <da.olivera@mensajerosurbanos.com>
2021-12-22 12:41:02 -0500

    End course

--- docker_commands.sh
+++ docker_commands.sh
@@ -6,3 +6,9 @@ docker network create --attachable platzinet
 docker network connect platzinet db
 docker run -d --name app -p 3000:3000 --env MONGO_URL=mongodb://db:27017/test platziapp
 docker network connect platzinet app
+
+#Docker in docker
+
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock docker:19.03.12 
+# docker ps
+# docker run -d --name app prodapp
